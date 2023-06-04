f5ac5f6fc0be7312440537858e633207fc92511c
CodingStorageSoo <chlqudtn347@gmail.com>
2023-06-03 17:46:06 -0700

    week6

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -4,12 +4,23 @@ const Image = require("../models/Image");
 const { upload } = require("../middleware/imageUpload");
 
 imageRouter.post("/", upload.single("imageTest"), async (req, res) => {
-  console.log(req.file);
-  const image = await new Image({
-    key: req.file.filename,
-    originalFileName: req.file.originalname,
-  }).save();
-  res.json(image);
+  try {
+    if (!req.user) throw new Error("No Authorization");
+    const image = await new Image({
+      user: {
+        _id: req.user.id,
+        name: req.user.name,
+        username: req.user.username,
+      },
+      public: req.body.public,
+      key: req.file.filename,
+      originalFileName: req.file.originalname,
+    }).save();
+    res.json(image);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  }
 });
 
 imageRouter.get("/", async (req, res) => {
